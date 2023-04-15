eb5d7ab025b3cb7f624a3b836be7ab5097b2e141
Kanav <kanav@ucsb.edu>
2023-04-15 16:14:25 -0700

    “”

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -3,7 +3,6 @@
 // This provides exception types:
 #include <stdexcept>
 
-
 FibVec* create_fibvec() {
   // Used by the autograder to make a new FibVec.
   // You shouldn't need to change this.
@@ -12,3 +11,41 @@ FibVec* create_fibvec() {
 
 
 // FibVec Function Implementations
+FibVec::FibVec() {
+  v = new int[1]; // create buffer with capacity f(2) = 1
+  v_capacity= 1;
+  v_count = 0;
+}
+
+FibVec::~FibVec() {
+  delete[] v;
+}
+ 
+// Member Functions
+size_t FibVec::capacity() const {
+  return v_capacity;
+}
+
+size_t FibVec::count() const {
+  return v_count;
+}
+
+void FibVec::insert(int value, size_t index) {
+}
+
+
+void FibVec::push(int value) {
+  insert(value, v_count);
+}
+
+int FibVec::lookup(size_t index) const {
+  return 1;
+}
+
+int FibVec::pop() {
+  return 1;
+}
+
+int FibVec::remove(size_t index) {}
+
+  //
--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -1,22 +1,29 @@
 #ifndef FIBVEC_H
 #define FIBVEC_H
 
-// This provides the size_t type:
 #include <cstddef>
+#include <stdexcept>
 
 class FibVec {
-  // Member Variables
-
-  // Helper Functions
-
 public:
-  // Constructor and Destructor
+  FibVec();
+  ~FibVec();
+ 
+  size_t capacity() const;
+  size_t count() const;
+  void insert(int value, size_t index);
+  void push(int value);
+  int lookup(size_t index) const;
+  int pop();
+  int remove(size_t index);
 
-  // Member Functions
-
+private:
+  int* v;
+  size_t v_capacity;
+  size_t v_count;
+  size_t next_fib(size_t n) const;
 };
 
-// Used by the autograder:
 FibVec* create_fibvec();
 
 #endif
