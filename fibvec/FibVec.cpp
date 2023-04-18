cb04174bdc658a09ce6f75859978dd2702bfb24e
Kanav <kanav@ucsb.edu>
2023-04-18 14:29:53 -0700

    Your commit message here

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -89,8 +89,8 @@ int FibVec::remove(size_t index) {
   }
   v_count = v_count - 1;
 
-  if (v_count < next_fib(v_capacity -2)) {
-    cap_change(next_fib(v_capacity -1));
+  if (v_count < next_fib(v_capacity - 2)) {
+    cap_change(next_fib(v_capacity - 3));
   }
 
   return value;
