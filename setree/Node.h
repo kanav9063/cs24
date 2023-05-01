8cc0f036397652028ae5a9b53a41cf534b668262
Kanav <kanav@ucsb.edu>
2023-05-01 00:43:12 -0700

    lol

--- setree/Node.h
+++ setree/Node.h
@@ -1,7 +1,14 @@
 #ifndef NODE_H
 #define NODE_H
 
-// Use this file to declare your Node type.
-// Implement Node member functions and helper functions in Node.cpp.
+#include <string>
+
+class Node {
+  std::string mValue;
+  Node* mLeft;
+  Node* mRight;
+  size_t mSize;
+
+};
 
 #endif
--- setree/Set.cpp
+++ setree/Set.cpp
@@ -1,1 +1,52 @@
 #include "Set.h"
+
+
+Set::Set() {
+    mRoot=nullptr;
+}
+
+Set::Set(const Set& other) {
+    if (other.mRoot == nullptr) {
+        mRoot = nullptr;
+    } else {
+        mRoot = new Node(*other.mRoot);
+    }
+}
+
+Set::Set(Set&& other) {
+
+}
+
+Set::~Set() {
+    clear();
+}
+
+size_t Set::clear() {
+    return 1;
+}
+
+bool Set::contains(const std::string& value) const {
+    return false;
+}
+
+size_t Set::count() const {
+    return 1;
+}
+
+void Set::debug() {}
+
+
+
+size_t Set::insert(const std::string& value) {
+    return 1;
+}
+
+const std::string& Set::lookup(size_t n) const {
+    return " ";
+}
+
+void Set::print() const {}
+
+size_t Set::remove(const std::string& value) {
+    return 1;
+}
