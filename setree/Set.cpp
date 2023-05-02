80e24881488e5f092bdb45da55b5c1b1fda11945
Kanav <kanav@ucsb.edu>
2023-05-01 20:08:58 -0700

    added

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -5,6 +5,7 @@ Set::Set() {
     mRoot=nullptr;
 }
 
+
 Node* Set::copy(Node*& ptr, Node* otherNode) {
     if (otherNode == nullptr) {
         ptr = nullptr;
@@ -15,14 +16,17 @@ Node* Set::copy(Node*& ptr, Node* otherNode) {
     ptr->right = copy(ptr->right, otherNode->right);
     return ptr;
 }
-
 Set::Set(const Set& other) {
     mRoot = copy(mRoot, other.mRoot);
 }
 
-Set::Set(Set&& other) {
 
+Set::Set(Set&& other) {
+    mRoot=other.mRoot;
+    other.mRoot=nullptr; 
 }
+
+
 Set::~Set() {
     clear();
 }
@@ -40,6 +44,8 @@ size_t Set::clear() {
     return count;
 }
 
+
+
 bool containsHelper(const Node* root, const std::string& value) {
     if (root == nullptr) {
         return false;
@@ -51,16 +57,30 @@ bool containsHelper(const Node* root, const std::string& value) {
         return containsHelper(root->right, value);
     }
 }
-
 bool Set::contains(const std::string& value) const {
     return containsHelper(mRoot, value);
 }
 
-size_t Set::count() const {
-    return 1;
+
+
+size_t countHelper(Node* root, const std::string& value) const {
+    if (root == nullptr) {
+        return 0;
+    } else if (root->value == value) {
+        return 1 + countHelper(root->left, value) + countHelper(root->right, value);
+    } else if (value < root->value) {
+        return countHelper(root->left, value);
+    } else {
+        return countHelper(root->right, value);
+    }
+}
+size_t Set::count(const std::string& value) const {
+    return countHelper(mRoot, value);
 }
 
+
 void Set::debug() {}
+
 
 
 size_t insertHelper(Node*& root, const std::string& value) {
@@ -74,10 +94,10 @@ size_t insertHelper(Node*& root, const std::string& value) {
     }
     return 0; 
 }
-
 size_t Set::insert(const std::string& value) {
     return insertHelper(mRoot, value);
 }
+
 
 const std::string& Set::lookup(size_t n) const {
     static std ::string s("");
