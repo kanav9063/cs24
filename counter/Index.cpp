77ca236b1a5fb38bb7a4e65af0ba4c0aa952a91e
Kanav <kanav@ucsb.edu>
2023-05-17 22:44:33 -0700

    added Fibvec.cpp

--- counter/Index.cpp
+++ counter/Index.cpp
@@ -1,3 +1,16 @@
 #include "Index.h"
 
-// Index Member Functions
+void Index::addDocument(const std::vector<std::string>& words) {
+    for (int i = 0; i < words.size(); i++) {
+        const std::string& word = words[i];
+        indexMap[word].push_back(i);
+    }
+}
+
+std::vector<int> Index::search(const std::string& word) const {
+    auto it = indexMap.find(word);
+    if (it != indexMap.end()) {
+        return it->second;
+    }
+    return std::vector<int>();
+}
