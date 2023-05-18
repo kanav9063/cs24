f50b055dd721e9c427f18bd4fcb5a554caf32293
Kanav <kanav@ucsb.edu>
2023-05-18 09:10:39 -0700

    added Fibvec.cpp

--- counter/Index.cpp
+++ counter/Index.cpp
@@ -1,3 +1,79 @@
 #include "Index.h"
+#include <list>
+#include <vector>
+#include <string>
 
 // Index Member Functions
+
+Index::Index() : table(numBuckets) {}
+
+// Insert a key-value pair into the hash table
+void Index::insert(const std::string& key, int value) {
+    int index = hashFunction(key);
+    std::list<KeyValuePair>& bucket = table[index];
+
+    // Check if the key already exists in the bucket
+    for (auto& pair : bucket) {
+        if (pair.key == key) {
+            pair.value = value;
+            return;
+        }
+    }
+
+    // Key doesn't exist, add a new key-value pair
+    KeyValuePair newPair;
+    newPair.key = key;
+    newPair.value = value;
+    bucket.push_back(newPair);
+}
+
+// Remove a key-value pair from the hash table
+void Index::remove(const std::string& key) {
+    int index = hashFunction(key);
+    std::list<KeyValuePair>& bucket = table[index];
+
+    // Search for the key in the bucket
+    for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+        if (it->key == key) {
+            bucket.erase(it);
+            return;
+        }
+    }
+}
+
+// Get the value associated with a key in the hash table
+int Index::get(const std::string& key) {
+    int index = hashFunction(key);
+    std::list<KeyValuePair>& bucket = table[index];
+
+    // Search for the key in the bucket
+    for (const auto& pair : bucket) {
+        if (pair.key == key) {
+            return pair.value;
+        }
+    }
+
+    // Key not found
+    return -1;
+}
+
+
+// Update the value associated with a key in the hash table
+void Index::update(const std::string& key, int newValue) {
+    int index = hashFunction(key);
+    std::list<KeyValuePair>& bucket = table[index];
+
+    // Search for the key in the bucket
+    for (auto& pair : bucket) {
+        if (pair.key == key) {
+            pair.value = newValue;
+            return;
+        }
+    }
+
+    // Key not found, insert a new key-value pair
+    KeyValuePair newPair;
+    newPair.key = key;
+    newPair.value = newValue;
+    bucket.push_back(newPair);
+}
--- counter/Index.h
+++ counter/Index.h
@@ -5,4 +5,38 @@
 // This can make things simpler by clearly separating functionality.
 // The Index is in charge of getting pairs from the DataStore by key.
 
+
+class Index {
+private:
+    // Define the structure for key-value pair
+    struct KeyValuePair {
+        std::string key;
+        int value;
+    };
+
+    // Number of buckets in the hash table
+    static const int numBuckets = 26;
+
+    // Array of doubly linked lists (buckets)
+    std::vector<std::list<KeyValuePair>> table;
+
+    // Hash function to map a key to an index
+    int hashFunction(const std::string& key) {}
+
+public:
+    Index() : table(numBuckets) {}
+
+    // Insert a key-value pair into the hash table
+    void insert(const std::string& key, int value) {}
+
+    // Remove a key-value pair from the hash table
+    void remove(const std::string& key) {}
+
+    // Get the value associated with a key in the hash table
+    int get(const std::string& key) {}
+    
+    // Update the value associated with a key in the hash table
+    void update(const std::string& key, int newValue) {}
+};
+
 #endif
