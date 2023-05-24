462ee959b1a3b56ffc2a81a0a05082a01e60a201
Kanav <kanav@ucsb.edu>
2023-05-24 16:44:30 -0700

    added Fibvec.cpp

---
+++ typo/WordList.cpp
@@ -0,0 +1,64 @@
+#include "WordList.h"
+#include <istream>
+#include <string>
+
+WordList::WordList(std::istream& stream)
+{
+    std::string word;
+    while (std::getline(stream, word))
+    {
+        if (word[0] == '#')
+        {
+            continue;
+        }
+
+        if (word[word.length() - 1] == '\r')
+        {
+            word = word.substr(0, word.size() - 1);
+        }
+
+        bool isValidWord = true;
+        for (char c : word)
+        {
+            if (c < 'a' || c > 'z')
+            {
+                isValidWord = false;
+                break;
+            }
+        }
+
+        if (isValidWord)
+        {
+            mWords.push_back(word);
+        }
+    }
+}
+
+Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float threshold) const
+{
+    Heap heap(maxcount);
+
+    for (const std::string& word : mWords)
+    {
+        if (word.length() != points.size())
+        {
+            continue;
+        }
+
+        float score = 0;
+        for (size_t i = 0; i < word.size(); i++)
+        {
+            const Point& p = QWERTY[word[i] - 'a'];
+            float distanceSquared = (points[i].x - p.x) * (points[i].x - p.x) + (points[i].y - p.y) * (points[i].y - p.y);
+            score += 1.0f / (10.0f * distanceSquared + 1);
+        }
+
+        score /= word.size();
+        if (score >= threshold)
+        {
+            heap.push(word, score);
+        }
+    }
+
+    return heap;
+}
